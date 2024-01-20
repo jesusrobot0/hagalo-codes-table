2b2b80de0515545b40c259d12143992ad0f0701b
jesusrobot0 <jesusrobot9000@icloud.com>
2024-01-20 00:28:31 -0600

    Update copy on YouAreHere componet

--- src/components/you-are-here/YouAreHere.tsx
+++ src/components/you-are-here/YouAreHere.tsx
@@ -5,7 +5,7 @@ import { usePathname } from "next/navigation";
 export function YouAreHere() {
   const currentPath = usePathname();
   const currentPage =
-    currentPath === "/list" ? "Listado de productos" : "Formulario de producto";
+    currentPath === "/list" ? "Listado de productos" : "Agregar producto";
 
   return (
     <span className="text-gray-500 hidden sm:inline-block">{currentPage}</span>
