1fd6a7f6f1c456645cb16277795184b23d50e440
jesusrobot0 <jesusrobot9000@icloud.com>
2024-01-29 18:27:30 -0600

    Add next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {"headers": [
+  {
+    "source": "/api/:path*",
+    "headers": [
+      {
+        "key": "Access-Control-Allow-Origin",
+        "value": "*",
+      },
+    ],
+  },
+],};
 
 export default nextConfig;
