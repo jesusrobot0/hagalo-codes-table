dc9723a630d60a56b195eb12527049c46a99c014
jesusrobot0 <jesusrobot9000@icloud.com>
2024-03-02 23:56:43 -0600

    Add the feature of item selector per page

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,13 +1,12 @@
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
-import { TitlePage, Table, ProductsTable } from "@/components";
+import { TitlePage, Table } from "@/components";
 
 export default async function ProductListPage() {
   return (
     <>
       <TitlePage title="Lista de productos" />
-      {/* <ProductsTable /> */}
       <Table />
     </>
   );
--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -35,10 +35,15 @@ export function Table() {
     getPaginationRowModel: getPaginationRowModel(),
     getFilteredRowModel: getFilteredRowModel(),
     state: { globalFilter: filtering },
+    initialState: {
+      pagination: {
+        pageSize: 8,
+      },
+    },
     onGlobalFilterChange: setFiltering,
   });
 
-  // Meéodos de la paginación
+  // Méodos de la paginación
   const handlePrevious = () => {
     table.previousPage();
   };
@@ -52,6 +57,7 @@ export function Table() {
     table.nextPage();
   };
 
+  // Métodos de la busqueda
   const handleSearch = (e: FormEvent) => {
     e.preventDefault();
     setFiltering((e.target as HTMLInputElement).value);
@@ -74,28 +80,53 @@ export function Table() {
           <input
             type="text"
             placeholder="Buscar en la tabla"
-            className="w-[320px] p-3 border-l border-l-[#ccc] focus:outline-none  lg:w-[420px]"
+            className="w-[320px] p-2 md:p-3 border-l border-l-[#ccc] focus:outline-none  lg:w-[420px]"
             value={filtering}
             onChange={handleSearch}
           />
         </div>
-        <div className="flex items-center gap-4 border border-[#ccc] bg-white">
-          <button
-            onClick={handlePrevious}
-            className="border-r border-r-[#ccc] p-3"
-          >
-            <ChevronLeft color="#283d69" />
-          </button>
-          <p className="p-3 text-gray-400">
-            <span className="text-gray-800">
-              {table.getState().pagination.pageIndex + 1}
+
+        <div className="flex items-center gap-4">
+          <div className="bg-white p-2 md:p-3 border border-[#ccc]">
+            <span className="text-gray-400">
+              Items <span className="hidden md:visible"> por página:</span>
             </span>
-            <span> de </span>
-            {table.getPageCount()}
-          </p>
-          <button onClick={handleNext} className="border-l border-l-[#ccc] p-3">
-            <ChevronRight color="#283d69" />
-          </button>
+            <select
+              className="focus:outline-none cursor-pointer"
+              value={table.getState().pagination.pageSize}
+              onChange={(e) => {
+                table.setPageSize(Number(e.target.value));
+              }}
+            >
+              {[2, 4, 8, 16, 32].map((pageSize) => (
+                <option key={pageSize} value={pageSize}>
+                  {pageSize}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          <div className="flex items-center gap-4 border border-[#ccc] bg-white">
+            <button
+              onClick={handlePrevious}
+              className="border-r border-r-[#ccc] p-2 md:p-3 "
+            >
+              <ChevronLeft color="#283d69" />
+            </button>
+            <p className="p-2 md:p-3  text-gray-400">
+              <span className="text-gray-800">
+                {table.getState().pagination.pageIndex + 1}
+              </span>
+              <span> de </span>
+              {table.getPageCount()}
+            </p>
+            <button
+              onClick={handleNext}
+              className="border-l border-l-[#ccc] p-2 md:p-3 "
+            >
+              <ChevronRight color="#283d69" />
+            </button>
+          </div>
         </div>
       </div>
       <table className="w-full mb-6 border border-[#ccc]">
